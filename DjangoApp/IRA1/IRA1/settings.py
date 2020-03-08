3ee286f9de6f50e013f4c0df6030f8c10d710714
hassan11196 <hassan11196@hotmail.com>
2020-03-09 01:19:03 +0500

    set static root

--- DjangoApp/IRA1/IRA1/settings.py
+++ DjangoApp/IRA1/IRA1/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:3000'
